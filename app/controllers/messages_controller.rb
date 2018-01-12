b607c7655b7b1d5197109376641fc75d751dd0c4
EC2 Default User <ec2-user@ip-172-31-3-2.us-east-2.compute.internal>
2018-01-12 05:50:03 +0000

    DRY

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,57 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  def index
+    @messages = Message.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @message = Message.new
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    
+    if @message.save
+      flash[:success] = 'Messageが正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Messageが投稿できませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @massage.update(message_params)
+      flash[:success] = 'メッセージは正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Messageは更新されませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    
+    flash[:success] = 'Messageは正常に削除されました'
+    redirect_to messages_url
+  end
+  
+  private
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+  
+  #Storong Parameter
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'messages#index'
+  # get 'messages/:id', to: 'messaages#show', as: 'message'
+  resources :messages
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'messages/:id', to: 'messaages#show'
+  # post 'messages', to: 'messages#create'
+  # put 'messages/:id', to: 'messages#update'
+  # delete 'messages/:id', to: 'messages#destroy'
+  
+  # #index: show の補助ページ
+  # get 'messages', to: 'message#index'
+  
+  # # new: 新規作成用のフォームページ
+  # get 'message/new', to: 'messages#new'
+  
+  # # edit: 更新用のフォームページ
+  # get 'messages/:id/edit', to: 'message#edit'
 end
